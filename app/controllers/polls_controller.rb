19a0b7598d4873e64d4e76ee4c617f03ee00e0af
diego boy <diegolive5000@gmail.com>
2021-11-19 19:01:59 +0100

    add new and create poll actions and respective view

---
+++ app/controllers/polls_controller.rb
@@ -0,0 +1,30 @@
+class PollsController < ApplicationController
+  def index
+    @polls = Poll.all
+  end
+
+  def show
+    @poll = Poll.find(params[:id])
+  end
+
+  def new
+    @poll = Poll.new
+  end
+  
+  def create
+    @poll = Poll.new(poll_params)
+
+    if @poll.save
+      redirect_to @poll
+    else
+      render :new
+    end
+  end
+  
+  private
+
+  def poll_params
+    params.require(:poll).permit(:title)
+  end
+  
+end
