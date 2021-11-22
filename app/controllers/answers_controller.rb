0a0180326ce07b413eca361448d0147079436f39
diego boy <diegolive5000@gmail.com>
2021-11-22 18:26:31 +0100

    add gem acts_as_votable

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,4 +1,16 @@
 class AnswersController < ApplicationController
+  def upvote
+    @poll = Poll.find(params[:poll_id])
+    @answer = @poll.answers.find(params[:id])
+    if current_user.voted_up_on? @answer
+      @answer.unvote_by current_user
+    else
+      @answer.upvote_by current_user
+    end
+    # render "vote.js.erb"
+  end
+  
+
   def create
     @poll = Poll.find(params[:poll_id])
     @answer = @poll.answers.create(answer_params)
