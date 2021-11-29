f1fcb50a41be987de503964ae475510bb6c9122b
diego boy <diegolive5000@gmail.com>
2021-11-29 21:46:55 +0100

    added some general styling to the page

--- app/javascript/graph.js
+++ app/javascript/graph.js
@@ -2,31 +2,31 @@ document.addEventListener('turbolinks:load', () => {
 
     const votes = document.querySelectorAll(".answer-votes");
     const answers = document.querySelectorAll(".answer-title");
-  
+
     let voteArr = [];
     votes.forEach( (vote) => {
-      voteArr.push(vote.textContent);
+        voteArr.push(vote.textContent);
     })
     console.log(voteArr);
-  
+
     let answerArr = [];
     answers.forEach( (answer) => {
-      answerArr.push(answer.textContent);
+        answerArr.push(answer.textContent);
     })
     console.log(answerArr);
-  
-  
+
+
     const ctx = document.getElementById('myChart');
     const myChart = new Chart(ctx, {
-      type: 'bar',
-      data: {
+        type: 'bar',
+        data: {
         labels: answerArr,
         datasets: [{
-          label: '# of Votes',
-          data: voteArr,
-          borderWidth: 1
+            label: '# of Votes',
+            data: voteArr,
+            borderWidth: 1
         }]
-      },
+        },
     });
       
-  })
+})
